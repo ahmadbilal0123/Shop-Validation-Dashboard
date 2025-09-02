295748ae47dcb80093a5a09c1aed91504204d89a
gobnil <shahfahadhjk@gmail.com>
2025-09-02 18:15:28 +0500

    dashboard name chnage

--- frontend/components/dashboard-sidebar.tsx
+++ frontend/components/dashboard-sidebar.tsx
@@ -24,7 +24,7 @@ import { useState } from "react"
 const navigation = [
   { name: "Dashboard", href: "/dashboard", icon: LayoutDashboard },
   {
-    name: "Auditors",
+    name: "Shop Details",
     icon: Users,
     children: [
       { name: "Visits", href: "/dashboard/visits", icon: MapPin },
